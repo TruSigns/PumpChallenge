528fbf08c15394891113b902ebb994e3f80254fb
TruSigns <ReeseRuffin>
2022-01-21 14:16:52 -0500

    Still needs more work

--- src/cademy/training/ChartData.java
+++ src/cademy/training/ChartData.java
@@ -2,17 +2,63 @@ package cademy.training;
 
 import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 public class ChartData {
     //lv22 double charts
-    public String[] walkInThePark = {};
+    public String[] walkInThePark = {"CanCan", "Cleaner", "Meteo5cience", "FFF22", "Wedding Crasher", "Hyponosis22", "Redline", "1950", "Monolith", "Just hold on", "Revolution", ""};
     //lv23 double charts
     private String[] timeToSuffer = {"Travel To Future", "Nililism", "Creed", "HTTP", "Cross Over", "Final Autition 2-1", "Love is a Danger Zone", "Gargoyle", "Broken Karma", "BS Explosion", "Windmill", "Prime Time", "Clematis", "Nyar", "Your Mind", "D&D", "Red Snow", "Stardream", "Crossing Delta", "Video Out C"};
     //lv24 double charts
-    private String[] chooseDeath = {"la Cinq", "Gloria", "Vanish", "Harma", "Conflict", "Sarabande", "Bedlam", "Final Audition", "Achluoias", "FFF", "Full moon", "Full moon FULL", "Annihilator", "Creed FULL", "BrainPower", "lolite", "Dement", "Destri", "Cross Soul", "TFTMN FULL", "Errorcode", "Dignity", "A Site De La Rue", "Trashy", "Paved Garden", "V3"};
+    private String[] chooseDeath = {"la Cinq", "Gloria", "Vanish", "Harma", "Conflict", "Sarabande", "Bedlam", "Final Audition", "Achluoias", "FFF24", "Full moon", "Full moon FULL", "Annihilator", "Creed FULL", "BrainPower", "lolite", "Dement", "Destri", "Cross Soul", "TFTMN FULL", "Errorcode", "Dignity", "A Site De La Rue", "Trashy", "Paved Garden", "V3"};
 
     Random r = new Random();
+    Scanner sc = new Scanner(System.in);
+
+    public void select(){
+
+        System.out.println("Please choose from the following: \n 1. WalkInThePark(lv22charts) \n 2. timeToSuffer(lv23 Double Charts) \n 3. chooseDeath(lv24 double charts)");
+        int select = sc.nextInt();
+
+        switch (select){
+            case 1:
+                System.out.println("You have chosen chill and the following random charts are:");
+                chill();
+                break;
+            case 2:
+                System.out.println("I see you trying to sweat and chosen suffer. The following charts you will play are:");
+                suffer();
+                break;
+            case 3:
+                System.out.println("You decided to not B**** out. Here's your list:");
+                death();
+                break;
+        }
+
+    }
+
+
+    public void chill(){
+
+        Set<String> set = new HashSet<>();
+        for(int i = 0 ; i < 4 ; i++){
+            int r_int = r.nextInt(11);
+            String chooseChill = (walkInThePark[r_int]);
+            if(!set.contains(chooseChill)){
+                set.add(chooseChill);
+                System.out.println(chooseChill);
+            }else{
+                i--;
+                continue;
+            }
+
+
+        }
+
+
+
+        }
 
 
     public void suffer(){
--- src/cademy/training/Main.java
+++ src/cademy/training/Main.java
@@ -8,27 +8,12 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        // The pump data consist of ONLY D24 PumpItUp charts
 
-        Random rand = new Random();
-
-        ChartData novice = new ChartData();
-        novice.death();
+        ChartData chartData = new ChartData();
+        chartData.select();
 
 
-//
-//        Set<String> set = new HashSet<>();
-//        for (int i = 0; i < 4; i++) {
-//            int rand_int = rand.nextInt(26);
-//            String chooseDeath = (PumpData[rand_int]);
-//            if (!set.contains(chooseDeath)) {
-//                set.add(chooseDeath);
-//                System.out.println(chooseDeath);
-//            } else {
-//                i--;
-//                continue;
-//            }
-//        }
+
 
 
     }
